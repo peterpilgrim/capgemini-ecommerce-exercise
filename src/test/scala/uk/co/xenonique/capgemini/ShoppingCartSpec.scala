6aeedfafd4ea5189da8fb820ffdc749955351882
peterpilgrim <peter.pilgrim@gmail.com>
2017-07-13 17:01:05 +0100

    Prepend copyright info

--- src/test/scala/uk/co/xenonique/capgemini/ShoppingCartSpec.scala
+++ src/test/scala/uk/co/xenonique/capgemini/ShoppingCartSpec.scala
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2017 by Peter Pilgrim, Milton Keynes, P.E.A.T UK LTD
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Creative Commons 3.0
+ * Non Commercial Non Derivation Share-alike License
+ * https://creativecommons.org/licenses/by-nc-nd/4.0/
+ *
+ * Developers:
+ * Peter Pilgrim -- design, development and implementation
+ *               -- Blog: http://www.xenonique.co.uk/blog/
+ *               -- Twitter: @peter_pilgrim
+ *
+ * Contributors:
+ *
+ *******************************************************************************/
 package uk.co.xenonique.capgemini
 
 import org.junit.runner.RunWith
