3cd2f479e5f60680534cfc5ed671e3d3c424e8a9
peterpilgrim <peter.pilgrim@gmail.com>
2017-07-13 15:22:35 +0100

    Make reasonable assumptions about the input to my solution. Accept a list of strings as input.

--- src/main/scala/uk/co/xenonique/capgemini/ShoppingCart.scala
+++ src/main/scala/uk/co/xenonique/capgemini/ShoppingCart.scala
@@ -6,9 +6,36 @@ package uk.co.xenonique.capgemini
   * @author Peter Pilgrim (peter)
   */
 
-class ShoppingCart(val items: List[CartItem] ) {
+class ShoppingCart(val items: List[CartItem]) {
 
-  def price(): BigDecimal =  items.map(x => x.price).sum
+  def price(): BigDecimal = items.map(x => x.price).sum
 
 }
 
+
+object ShoppingCart {
+  /**
+    * Converts list of String matching {@link CartItem} types to Scala case classes
+    * @param cartItems list of cart item types
+    */
+  def convert(cartItems: List[String]): ShoppingCart = {
+    val list = cartItems.map {
+      name =>
+        name.toLowerCase() match {
+          case "orange" => Orange()
+          case "apple" => Apple()
+          case _ => throw new NoSuchElementException("Sorry I don't know this product!")
+        }
+    }
+    new ShoppingCart(list)
+  }
+
+  /**
+    * Converts list of String matching {@link CartItem} types to Scala case classes
+    * @param productText a text representation of a list of cart items
+    */
+  def convert(productText: String): ShoppingCart = {
+    val list = productText.split("\\s+").toList
+    convert(list)
+  }
+}
--- src/test/scala/uk/co/xenonique/capgemini/ShoppingCartSpec.scala
+++ src/test/scala/uk/co/xenonique/capgemini/ShoppingCartSpec.scala
@@ -15,7 +15,7 @@ import scala.collection.immutable.List
 @RunWith(classOf[JUnitRunner])
 class ShoppingCartSpec extends FlatSpec with Matchers{
 
-  "A Shopping cart" should "calculate price of items in a list" in {
+  "A shopping cart" should "calculate price of items in a list" in {
     new ShoppingCart( List() ).price() should === (0.0)
     new ShoppingCart( List( Orange()) ).price() should === (0.25)
     new ShoppingCart( List( Apple()) ).price() should === (0.60)
@@ -23,6 +23,43 @@ class ShoppingCartSpec extends FlatSpec with Matchers{
     new ShoppingCart( List( Orange(), Apple(), Orange()) ).price() should === (1.10)
     new ShoppingCart( List( Apple(), Orange(), Orange(), Apple()) ).price() should === (1.70)
     new ShoppingCart( List( Apple(), Apple(), Orange(), Apple()) ).price() should === (2.05)      /* !!! */
+  }
+
+  "Shopping cart" should "calculate price of items in a list" in {
+    new ShoppingCart( List() ).price() should === (0.0)
+    new ShoppingCart( List( Orange()) ).price() should === (0.25)
+    new ShoppingCart( List( Apple()) ).price() should === (0.60)
+    new ShoppingCart( List( Orange(), Orange(), Apple()) ).price() should === (1.10)
+    new ShoppingCart( List( Orange(), Apple(), Orange()) ).price() should === (1.10)
+    new ShoppingCart( List( Apple(), Orange(), Orange(), Apple()) ).price() should === (1.70)
+    new ShoppingCart( List( Apple(), Apple(), Orange(), Apple()) ).price() should === (2.05)      /* !!! */
+  }
+
+
+  "A shopping cart" should "take a list of items as String text input" in {
+    val cart1 = ShoppingCart.convert( List("Apple","orange","apple"))
+    cart1.items should === (List(Apple(), Orange(), Apple()))
+
+    val cart2 = ShoppingCart.convert( List("orange","APPLE","Orange"))
+    cart2.items should === (List(Orange(), Apple(), Orange()))
+
+    val cart3 = ShoppingCart.convert( List())
+    cart3.items should === (List())
+
+    val cart4 = ShoppingCart.convert( "Apple APPLE Orange apple")
+    cart4.items should === (List(Apple(), Apple(), Orange(), Apple()))
+  }
+
+  "A shopping cart" should "barf with unrecognisable input" in {
+    assertThrows[NoSuchElementException] {
+      ShoppingCart.convert( List("Apple","DOG","CAT"))
+    }
+  }
+
+  "A shopping cart" should "calculate price according to the example" in {
+    ShoppingCart.convert( List( "Apple", "Apple", "Orange", "Apple") ).price() should === (2.05)      /* !!! */
+
+    ShoppingCart.convert( "Apple Apple Orange Apple" ).price() should === (2.05)      /* !!! */
   }
 
 }
